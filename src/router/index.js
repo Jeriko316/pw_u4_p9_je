78f676879ac5d2f7d642368ba5a8185a0e782946
Jhon Enriquez <jenrical@nttdata.com>
2026-02-05 18:44:33 -0500

    taller36

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,13 @@ import GuardarEstudiante from '@/views/GuardarEstudiante.vue'
 import ActualizarEstudiante from '@/views/ActualizarEstudiante.vue'
 import ActualizarParcial from '@/views/ActualizarParcial.vue'
 import BorrarEstudiante from '@/views/BorrarEstudiante.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
+  { path: '/', name: 'home', component: HomeView, meta:{requiereAutorizacion: true, esPublica: false} },
   { path: '/', redirect: '/mostrar-todos' },
-  { path: '/mostrar-todos', component: MostrarTodos },
-  { path: '/mostrar-por-id', component: MostrarPorId },
+  { path: '/mostrar-todos', meta:{requiereAutorizacion: true, esPublica: false} ,component: MostrarTodos },
+  { path: '/mostrar-por-id',meta:{requiereAutorizacion: true, esPublica: false} , component: MostrarPorId },
   { path: '/guardar', component: GuardarEstudiante },
   { path: '/actualizar', component: ActualizarEstudiante },
   { path: '/actualizar-parcial', component: ActualizarParcial },
@@ -21,6 +23,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+/*COnfiguracion del Guardian */
+router.beforeEach((to, from , next) =>{
+    if(to.meta.requiereAutorizacion){
+        /**le envio a una pagina de login */
+        console.log("Requiere ir a Login")
+    }else{
+      /**le dejo sin validacion */
+      console.log("pase libre")
+      next();
+    }
 })
 
 export default router
