4f4823a009f040f78f2dba4d8ee613a597092937
Jhon Enriquez <jenrical@nttdata.com>
2026-02-05 19:41:33 -0500

    taller 37

---
+++ src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <input v-model="usuario" type="text" placeholder="usuario">
+    <input v-model="password" type="password" placeholder="contraseña">
+    <button>Entrar</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+    data(){
+        return {
+            usuario:"",
+            password:"",
+        };
+    },
+    methods: {
+        login(){
+            /**mandar a obtener el token */
+            const TOKEN ="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJtYXRyaWN1bGEtYXV0aCIsImF1ZCI6Im1hdHJpY3VsYSIsInN1YiI6ImprIiwidXNlciI6ImprIiwiZ3JvdXBzIjpbImFkbWluIl0sImV4cCI6MTc3MDQyNDY3MywiaWF0IjoxNzcwMzM4MjczLCJqdGkiOiI2ZWE4MGYyMy0zZDIzLTQwOGUtYjBiMi0yNThkNTFkZGZjYmMifQ.kL2qWh-vtgtYj3e-clp52zr4Iit6uQUlYjnfHITrJ9bEQi2tXoGkB5sYIcTsL5zDkDA52jtrgo85QwEAdq1NwVzHDGZkl9DzL0q4o4yyi2EO1cb3z6CeBfdV6xndyd0TMScNABoVa2HHCJspZbNeEiNVjNLsNizf-OhoIzfYWev5QKzPLJ9aISiv4fO_-rFfc_5B56ypuKnraIsWpkoq59CR74CsnzbORgubGBGnvCaIGOtrtuRbVy_oxEU3uLxj_2Qpl5GfwyIumkDh-cb4zmSjl93TixwGl4bCxpaqGAOjGnOsIfkExCYorhdX4_3BHfvDPk2GLcC7cT10FapZTA";
+            if(TOKEN !=null){
+                localStorage.setItem("token",TOKEN);
+                localStorage.setItem("estaAutenticado", true)
+            }else{
+                console.log("Error de autenticacion")
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.login {
+    width: 300px;
+    margin: 100px;
+    padding: 20px;
+    border:1px solid #ddd;
+    border-radius: 8px;
+    text-align:  center;
+    
+
+}
+
+input {
+    width: 100%;
+    margin-bottom: 10px;
+    padding: 8px;
+
+}
+
+button {
+    width: 100px;
+    padding: 8px;
+    cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,64 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import MostrarTodos from '@/views/MostrarTodos.vue'
-import MostrarPorId from '@/views/MostrarPorId.vue'
-import GuardarEstudiante from '@/views/GuardarEstudiante.vue'
-import ActualizarEstudiante from '@/views/ActualizarEstudiante.vue'
-import ActualizarParcial from '@/views/ActualizarParcial.vue'
-import BorrarEstudiante from '@/views/BorrarEstudiante.vue'
-import HomeView from '@/views/HomeView.vue'
+import MostrarTodos from "@/views/MostrarTodos.vue";
+import MostrarPorId from "@/views/MostrarPorId.vue";
+import GuardarEstudiante from "@/views/GuardarEstudiante.vue";
+import ActualizarEstudiante from "@/views/ActualizarEstudiante.vue";
+import ActualizarParcial from "@/views/ActualizarParcial.vue";
+import BorrarEstudiante from "@/views/BorrarEstudiante.vue";
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
-  { path: '/', name: 'home', component: HomeView, meta:{requiereAutorizacion: true, esPublica: false} },
-  { path: '/', redirect: '/mostrar-todos' },
-  { path: '/mostrar-todos', meta:{requiereAutorizacion: true, esPublica: false} ,component: MostrarTodos },
-  { path: '/mostrar-por-id',meta:{requiereAutorizacion: true, esPublica: false} , component: MostrarPorId },
-  { path: '/guardar', component: GuardarEstudiante },
-  { path: '/actualizar', component: ActualizarEstudiante },
-  { path: '/actualizar-parcial', component: ActualizarParcial },
-  { path: '/borrar', component: BorrarEstudiante }
-]
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { requiereAutorizacion: true, esPublica: false },
+  },
+  { path: "/", redirect: "/mostrar-todos" },
+  { path: "/Login", name: "login", component: LoginView },
+  {
+    path: "/mostrar-todos",
+    meta: { requiereAutorizacion: true, esPublica: false },
+    component: MostrarTodos,
+  },
+  {
+    path: "/mostrar-por-id",
+    meta: { requiereAutorizacion: true, esPublica: false },
+    component: MostrarPorId,
+  },
+  { path: "/guardar", component: GuardarEstudiante },
+  { path: "/actualizar", component: ActualizarEstudiante },
+  { path: "/actualizar-parcial", component: ActualizarParcial },
+  { path: "/borrar", component: BorrarEstudiante },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
 /*COnfiguracion del Guardian */
-router.beforeEach((to, from , next) =>{
-    if(to.meta.requiereAutorizacion){
-        /**le envio a una pagina de login */
-        console.log("Requiere ir a Login")
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiereAutorizacion) {
+    /**le envio a una pagina de login */
+    const estaAutenticado = localStorage.getItem("estaAutenticado");
+    const token = localStorage.getItem("token");
+
+    if(!estaAutenticado){
+      console.log("Requiere ir a Login");
+      next({ name: "login" });
     }else{
-      /**le dejo sin validacion */
-      console.log("pase libre")
       next();
     }
-})
+    
+  } else {
+    /**le dejo sin validacion */
+    console.log("pase libre");
+    next();
+  }
+});
 
-export default router
+export default router;
